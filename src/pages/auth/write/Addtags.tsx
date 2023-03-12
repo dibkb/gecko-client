d4501e5b15dda5f7cbeac8a74ac0f67d4b6ced8e
dibkb <dibas9110@gmail.com>
2023-03-12 18:14:03 +0530

    add tag handler working

--- src/pages/auth/write/Addtags.tsx
+++ src/pages/auth/write/Addtags.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-const data = [
+import React, { useCallback, useState } from "react";
+const data: Record<"id" | "tag", string>[] = [
   { id: "1", tag: "Frontend" },
   { id: "2", tag: "Backend" },
   { id: "3", tag: "System Design" },
@@ -8,23 +8,36 @@ const data = [
   { id: "6", tag: "Computer Fundamentals" },
 ];
 const Addtags: React.FC = () => {
-  const tagContent = data.map(({ id, tag }) => {
+  const [selectedTags, setSelectedTags] = useState<
+    Record<"id" | "tag", string>[]
+  >([]);
+  const [alltags, setAllTags] = useState<Record<"id" | "tag", string>[]>(data);
+  const addTagHandler = useCallback((element) => {
+    setSelectedTags((prev) => [...prev, element]);
+    setAllTags((prev) => prev.filter((item) => item.id !== element.id));
+  }, []);
+  const tagContent = alltags.map((element) => {
     return (
       <span
-        key={id}
-        className="border border-primary rounded-full px-3 py-1 cursor-pointer"
+        key={element.id}
+        onClick={() => {
+          addTagHandler(element);
+        }}
+        className="border border-primary rounded-full px-4 py-1 cursor-pointer hover:bg-primary hover:text-white"
       >
-        {tag}
+        {element.tag}
       </span>
     );
   });
+
   return (
     <div>
       <h1 className="text-center text-4xl font-semibold">Add Tags</h1>
       <section className="flex flex-col mx-auto mt-24">
         <h3 className="text-center font-medium text-md">Selected Tags</h3>
+        {JSON.stringify(selectedTags)}
       </section>
-      <section className="mt-12 flex gap-x-6 gap-y-4 justify-center max-w-6xl mx-auto flex-wrap">
+      <section className="mt-12 flex gap-x-6 gap-y-4 justify-center max-w-6xl mx-auto flex-wrap select-none">
         {tagContent}
       </section>
     </div>
