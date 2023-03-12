2521e1e78b1e3e80b43e13346ebb59bb72ed925b
dibkb <dibas9110@gmail.com>
2023-03-12 12:23:58 +0530

    router setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Register from "./pages/Register";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Auth routes */}
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
