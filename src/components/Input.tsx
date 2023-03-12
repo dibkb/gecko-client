0eda33f4699ec602c6b109a41ed10ba15938d070
dibkb <dibas9110@gmail.com>
2023-03-12 13:12:03 +0530

    form input fields

---
+++ src/components/Input.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+interface InputProps {
+  id: string;
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
+}
+const Input: React.FC<InputProps> = ({
+  id,
+  placeholder,
+  value,
+  onChange,
+  type = "text",
+}) => {
+  return (
+    <div className="relative">
+      <input
+        id={id}
+        type={type}
+        className="border-2 border-gray-200 w-full rounded-md text-base outline-none
+              px-6
+              pt-6
+              pb-2
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        focus:border-primary
+        peer
+        invalid:border-b-1
+        font-medium
+         "
+        placeholder=" "
+        value={value}
+        onChange={onChange}
+      />
+      <label
+        htmlFor={id}
+        className="
+        absolute 
+        text-sm
+      text-zinc-600
+        duration-150 
+        transform 
+        -translate-y-3 
+        scale-75 
+        top-4 
+        z-9 
+        origin-[0] 
+        left-6
+        peer-placeholder-shown:scale-100 
+        peer-placeholder-shown:translate-y-0 
+        peer-focus:scale-75
+        peer-focus:-translate-y-3
+      "
+      >
+        {placeholder}
+      </label>
+    </div>
+  );
+};
+
+export default Input;
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "../../components/Input";
 const Register: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   return (
-    <div className="mx-auto w-fit">
+    <div className="mx-auto max-w-lg">
       <h1 className="text-4xl md:text-5xl font-semibold">Register</h1>
       <p className="my-8">Using our unlimited passion for technology</p>
+      <form action="submit" className="flex flex-col gap-y-4">
+        <Input
+          placeholder={"Username"}
+          id={"username"}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          type={"text"}
+        />
+        <Input
+          placeholder={"Name"}
+          id={"name"}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type={"text"}
+        />
+        <Input
+          placeholder={"Email"}
+          id={"email"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type={"text"}
+        />
+        <Input
+          placeholder={"Password"}
+          id={"password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type={"password"}
+        />
+      </form>
     </div>
   );
 };
