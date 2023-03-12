55e9981a0253d000ef7588a2a9979d066abebb4c
dibkb <dibas9110@gmail.com>
2023-03-12 12:53:16 +0530

    register page layout

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 const Register: React.FC = () => {
-  return <div>Register</div>;
+  return (
+    <div className="mx-auto w-fit">
+      <h1 className="text-4xl md:text-5xl font-semibold">Register</h1>
+      <p className="my-8">Using our unlimited passion for technology</p>
+    </div>
+  );
 };
 
 export default Register;
