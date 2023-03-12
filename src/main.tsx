f39a8a2a52371dd1448ee1c8f7aeba52ddb00f2e
dibkb <dibas9110@gmail.com>
2023-03-12 12:30:17 +0530

    auth routes setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,20 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Register from "./pages/Register";
+import Register from "./pages/auth/Register";
 import "./index.css";
+import AuthLayout from "./pages/auth/AuthLayout";
+import Login from "./pages/auth/Login";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         {/* Auth routes */}
-        <Route path="/register" element={<Register />} />
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="register" element={<Register />} />
+          <Route path="login" element={<Login />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+const Login: React.FC = () => {
+  return <div>Login</div>;
+};
+
+export default Login;
