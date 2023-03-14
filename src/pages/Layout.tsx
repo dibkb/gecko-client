0719bc246b173c9707f7bc3a7a83932b469f86e1
dibkb <dibas9110@gmail.com>
2023-03-14 12:27:41 +0530

    hamburger menu

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,8 +2,9 @@ import {
   UserCircleIcon,
   PencilSquareIcon,
   ArrowUpRightIcon,
+  Bars2Icon,
 } from "@heroicons/react/24/solid";
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import geckoLogo from "../assets/images/gecko-logo.png";
 import { useCurrentUser } from "../hooks/useCurrentUser";
@@ -39,11 +40,41 @@ export const UserLayout = () => {
   );
 };
 export const PublicLayout = () => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const hamburger = (
+    <main className="block sm:hidden cursor-pointer">
+      <Bars2Icon
+        className="h-6 w-6"
+        onClick={() => {
+          setShowMenu((prev) => !prev);
+        }}
+      />
+    </main>
+  );
+  const menuItems = (
+    <span className="absolute top-16 bg-white flex flex-col w-full gap-4 cursor-pointer text-sm font-medium">
+      <button className="flex gap-1 items-center mx-auto">
+        <PencilSquareIcon className="h-5 w-5 text-zinc-700" />
+        <p className="font-medium">Write</p>
+      </button>
+      <button className="px-4 py-2 border border-primary rounded-md hover:bg-zinc-100">
+        Login
+      </button>
+      <button
+        className="flex items-center gap-1 bg-primary hover:bg-black
+       text-white px-4 py-2 rounded-md transition duration-200 hover:scale-105 justify-center"
+      >
+        <p>Register</p>
+        <ArrowUpRightIcon className="h-4 w-4" />
+      </button>
+    </span>
+  );
   return (
     <div className="max-w-screen-2xl container mx-auto px-8 sm:px-6 lg:sm-4">
-      <nav className="py-4 select-none flex items-center justify-between">
+      <nav className="py-4 select-none flex items-center justify-between relative">
         {branding}
-        <span className="flex items-center gap-8 cursor-pointer text-sm font-medium">
+        {hamburger}
+        <span className="hidden sm:flex items-center gap-4 cursor-pointer text-sm font-medium">
           <button className="flex gap-1 items-center">
             <PencilSquareIcon className="h-5 w-5 text-zinc-700" />
             <p className="font-medium">Write</p>
@@ -58,6 +89,7 @@ export const PublicLayout = () => {
             </button>
           </main>
         </span>
+        {showMenu && menuItems}
       </nav>
       <Outlet />
     </div>
