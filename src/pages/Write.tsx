b84b8d10ce82d9151ee05bc1b07d655808aed566
dibkb <dibas9110@gmail.com>
2023-03-14 13:58:23 +0530

    write page redirect and button logic

--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,5 +1,10 @@
 import { ArrowUpRightIcon } from "@heroicons/react/24/solid";
-import React, { useEffect } from "react";
+import React, {
+  MouseEventHandler,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import Addphoto from "./write/Addphoto";
 import Addtags from "./write/Addtags";
 import Editpage from "./write/Editpage";
@@ -13,9 +18,13 @@ const Write: React.FC = () => {
       return navigate("/auth/login");
     }
   }, [currUser]);
-
+  const [page, setPage] = useState<number>(1);
+  // -------------------------------------BUTTONS--------------------------------
   const nextButton = (
     <button
+      onClick={() => {
+        setPage((prev) => prev + 1);
+      }}
       className="border font-medium bg-primary hover:bg-black text-white rounded-md px-12 py-2
      text-lg transition duration-300 ease-out hover:scale-105"
     >
@@ -24,29 +33,37 @@ const Write: React.FC = () => {
   );
   const prevButton = (
     <button
+      onClick={() => {
+        setPage((prev) => prev - 1);
+      }}
       className="border font-medium text-primary border-primary rounded-md px-12 py-2
      text-lg hover:bg-zinc-100"
     >
       Previous
     </button>
   );
-  const buttonConainer = (
-    <div className=" mt-6 mb-6 flex gap-12 justify-center">
-      {prevButton}
-      {nextButton}
-    </div>
-  );
   const publishButton = (
-    <button className="flex justify-center gap-2 items-center mt-24 mb-6 mx-auto border font-medium bg-primary hover:bg-black text-white rounded-md px-12 py-2 text-lg group transition duration-300 ease-out hover:scale-105">
+    <button
+      className="group flex gap-2 border font-medium bg-primary hover:bg-black text-white rounded-md px-12 py-2
+    text-lg transition duration-300 ease-out hover:scale-105"
+    >
       <p>Publish</p>
       <ArrowUpRightIcon className="h-6 w-6 text-white transition delay-300 duration-300 ease-out group-hover:rotate-[360deg] " />
     </button>
   );
+  const buttonConainer = (
+    <div className=" mt-6 mb-6 flex gap-12 justify-center">
+      {page !== 1 && prevButton}
+      {page !== 3 && nextButton}
+      {page === 3 && publishButton}
+    </div>
+  );
+  // -------------------------------------------------------------------------------
   return (
     <div>
-      <Editpage />
-      {/* <Addtags /> */}
-      {/* <Addphoto /> */}
+      {page === 1 && <Editpage />}
+      {page === 2 && <Addtags />}
+      {page === 3 && <Addphoto />}
       {buttonConainer}
     </div>
   );
