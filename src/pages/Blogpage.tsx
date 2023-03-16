e8e807eb92f2921e982e57199989d3b4dcfa3e81
dibkb <dibas9110@gmail.com>
2023-03-16 12:59:40 +0530

    emoji render

--- src/pages/Blogpage.tsx
+++ src/pages/Blogpage.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import likedEmoji from "../assets/images/liked.png";
+import lovedEmoji from "../assets/images/loved.png";
+import helpfulEmoji from "../assets/images/helpful.png";
 import { useParams } from "react-router-dom";
 import { useFetchBlogsByIdQuery } from "../app/blog/blogApiSlice";
 import { dateOptions } from "../utils/random";
@@ -6,8 +9,8 @@ const Blogpage = () => {
   const { blogId } = useParams();
   const { data, error, isLoading } = useFetchBlogsByIdQuery(blogId);
   return (
-    <div className="my-24">
-      <h1 className="text-center text-3xl font-semibold text-zinc-600">
+    <div className="my-8 md:my-24">
+      <h1 className="text-center text-3xl font-semibold text-zinc-700">
         {data?.title}
       </h1>
       <div className="flex gap-2 text-xs font-semibold text-zinc-500 w-fit mx-auto my-12">
@@ -23,8 +26,22 @@ const Blogpage = () => {
           backgroundPosition: "center",
           backgroundSize: "cover",
         }}
-        className={`mx-auto aspect-video border mb-4 rounded-lg max-h-[500px]`}
+        className={`mx-auto aspect-video rounded-lg max-h-[500px]`}
       ></div>
+      <div className="flex gap-4 justify-center mt-12">
+        <span className="flex gap-2 cursor-pointer items-center font-medium">
+          <img src={likedEmoji} alt="" />
+          <p className="">{data?.reaction.liked}</p>
+        </span>
+        <span className="flex gap-2 cursor-pointer items-center font-medium">
+          <img src={lovedEmoji} alt="" />
+          <p>{data?.reaction.loved}</p>
+        </span>{" "}
+        <span className="flex gap-2 cursor-pointer items-center font-medium">
+          <img src={helpfulEmoji} alt="" />
+          <p>{data?.reaction.helpful}</p>
+        </span>
+      </div>
       <p className="mt-12 leading-8">{data?.content}</p>
     </div>
   );
