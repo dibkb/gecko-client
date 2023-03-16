cffef9e2465ce020e8359ac53dbb7d767b670112
dibkb <dibas9110@gmail.com>
2023-03-16 20:05:20 +0530

    skeleton loader color cahnge

--- src/components/BlogSkeleton.tsx
+++ src/components/BlogSkeleton.tsx
@@ -1,23 +1,19 @@
 import React from "react";
-const textSkeleton = (
-  <p className="w-full h-4 rounded-md bg-zinc-200 skeleton"></p>
-);
+const textSkeleton = <p className="w-full h-4 rounded-md skeleton"></p>;
 const BlogSkeleton: React.FC = () => {
   return (
     <div className="my-8 md:my-24">
-      <h1 className="mx-auto bg-zinc-200 h-8 w-1/3 rounded-md skeleton"></h1>
+      <h1 className="mx-auto h-8 w-1/3 rounded-md skeleton"></h1>
       <div className="flex gap-4 my-12 justify-center">
         <p className="w-[84px] h-3 rounded-md skeleton"></p>
         <p className="w-[84px] h-3 rounded-md skeleton"></p>
       </div>
       <span className="flex gap-5 justify-center my-12">
-        <p className="w-20 h-5 rounded-full bg-zinc-200 skeleton"></p>
-        <p className="w-20 h-5 rounded-full bg-zinc-200 skeleton"></p>
-        <p className="w-20 h-5 rounded-full bg-zinc-200 skeleton"></p>
+        <p className="w-20 h-5 rounded-full skeleton"></p>
+        <p className="w-20 h-5 rounded-full skeleton"></p>
+        <p className="w-20 h-5 rounded-full skeleton"></p>
       </span>
-      <div
-        className={`mx-auto h-[500px] aspect-video bg-zinc-200 rounded-lg skeleton`}
-      ></div>
+      <div className={"mx-auto aspect-video rounded-lg skeleton"}></div>
       <div className="mt-12 flex flex-col gap-4">
         {[...Array(12)].map((e) => textSkeleton)}
       </div>
--- src/components/BlogcontainerSkeleton.tsx
+++ src/components/BlogcontainerSkeleton.tsx
@@ -3,14 +3,19 @@ import React from "react";
 export const BlogcontainerSkeleton: React.FC = () => {
   const div = (
     <div>
-      <div className={`aspect-video bg-zinc-200 mb-4 rounded-lg skeleton`} />
-      <p className="h-2 w-1/3 bg-zinc-200 skeleton rounded-md"></p>
-      <p className="h-4 w-full bg-zinc-200 skeleton my-4 rounded-md"></p>
-      <p className="h-16 w-full bg-zinc-200 skeleton my-4 rounded-md"></p>
+      <div className={`aspect-video  mb-4 rounded-lg skeleton`} />
+      <p className="h-2 w-1/3  skeleton rounded-md"></p>
+      <p className="h-4 w-full  skeleton my-4 rounded-md"></p>
+      <p className="h-16 w-full  skeleton my-4 rounded-md"></p>
       <span className="flex gap-3">
-        <p className="w-20 h-3 rounded-full bg-zinc-200 skeleton"></p>
-        <p className="w-20 h-3 rounded-full bg-zinc-200 skeleton"></p>
-        <p className="w-20 h-3 rounded-full bg-zinc-200 skeleton"></p>
+        <p className="w-20 h-4 rounded-full  skeleton"></p>
+        <p className="w-20 h-4 rounded-full  skeleton"></p>
+        <p className="w-20 h-4 rounded-full  skeleton"></p>
+      </span>
+      <span className="flex gap-3 mt-3">
+        <p className="w-5 h-3 rounded-full skeleton"></p>
+        <p className="w-5 h-3 rounded-full skeleton"></p>
+        <p className="w-5 h-3 rounded-full skeleton"></p>
       </span>
     </div>
   );
