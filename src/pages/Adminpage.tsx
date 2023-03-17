b57dc646a4781982fdaaf1c9bf725dfe9255809b
dibkb <dibas9110@gmail.com>
2023-03-17 17:19:12 +0530

    delete iocns redesign

--- src/pages/Adminpage.tsx
+++ src/pages/Adminpage.tsx
@@ -1,4 +1,4 @@
-import { TrashIcon } from "@heroicons/react/24/solid";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/solid";
 import React from "react";
 import { useParams } from "react-router-dom";
 import { useFetchBlogsByUserAdminQuery } from "../app/blog/blogApiSlice";
@@ -19,7 +19,7 @@ const Adminpage: React.FC = () => {
           />
           <button className="flex items-center gap-1 text-red-700 text-sm font-bold bg-red-100 rounded-lg px-6 py-2 hover:bg-red-200 mt-6">
             <p>Delete Account</p>
-            <TrashIcon className="h-4 w-4" />
+            <ExclamationTriangleIcon className="h-7 w-7 text-red-500" />
           </button>
           <BlogContainerAdmin data={data.blogs} />
         </>
