4bd0c099da85cff092db6de8bbb751045ef741b3
dibkb <dibas9110@gmail.com>
2023-03-18 13:54:16 +0530

    doggy image straingt from dribble

--- src/components/EmptyContainer.tsx
+++ src/components/EmptyContainer.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-import doggy from "../assets/images/doggo-min.png";
 import { WriteButton } from "./Button";
 const EmptyContainer = () => {
   return (
     <div className="flex flex-col items-center gap-6 pb-4 select-none">
-      <img src={doggy} alt="" className="h-xl w-fit" />
+      <img
+        src={
+          "https://cdn.dribbble.com/userupload/3074608/file/original-9383b4bfc24004f8dc5114b3d77402c7.jpg?compress=1&resize=1024x768"
+        }
+        alt=""
+        className="h-xl w-fit"
+      />
       <p className="text-stone-800 text-xl text-center">
         This section is empty. Maybe you can help to fill it up.
       </p>
--- src/components/Errorpage.tsx
+++ src/components/Errorpage.tsx
@@ -1,9 +1,14 @@
 import React from "react";
-import doggy from "../assets/images/doggo-min.png";
 const Errorpage: React.FC = () => {
   return (
     <div className="flex flex-col items-center gap-6 pb-4 select-none">
-      <img src={doggy} alt="" className="h-xl w-fit" />
+      <img
+        src={
+          "https://cdn.dribbble.com/userupload/3074608/file/original-9383b4bfc24004f8dc5114b3d77402c7.jpg?compress=1&resize=1024x768"
+        }
+        alt=""
+        className="h-xl w-fit"
+      />
       <p className="text-stone-800 text-3xl mb-10 text-center">
         Oops... Looks like something went wrong
       </p>
