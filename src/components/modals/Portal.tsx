acf74314fcad5b9a4da660e074f3cb559a0d7db2
dibkb <dibas9110@gmail.com>
2023-03-19 20:10:34 +0530

    New modal style

--- src/components/modals/Portal.tsx
+++ src/components/modals/Portal.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import { useNavigate } from "react-router-dom";
 import ReactDOM, { createPortal } from "react-dom";
 interface Portalinterface {
@@ -13,38 +14,123 @@ export const RegisterSuccessPortal: React.FC<Portalinterface> = ({
     setShowPortal(null);
     navigate("/auth/login");
   };
-  return ReactDOM.createPortal(
-    <div className="z-10 absolute h-full w-full bg-modalOverlay top-0">
-      <div className="select-none relative bg-white shadow-2xl w-fit mx-auto px-8 py-8 rounded-md top-12 flex flex-col">
-        <p className="font-medium">Your account has been created sucessfully</p>
-        <button
-          className="px-4 py-2 rounded-md bg-teal-600 text-white mx-auto mt-10 hover:bg-teal-800"
-          onClick={buttonClickHandler}
+  return (
+    <Transition appear show={true} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => setShowPortal(false)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          Login
-        </button>
-      </div>
-    </div>,
-    document.body
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-[50ch] items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  Account successfully created
+                </Dialog.Title>
+                <div className="mt-2">
+                  <p className="text-sm text-gray-500">
+                    Your account has been created sucessfully created at Helium.
+                    You can now login.
+                  </p>
+                </div>
+
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    onClick={buttonClickHandler}
+                  >
+                    Login
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
 export const FailurePortal: React.FC<Portalinterface> = ({
   setShowPortal,
   message,
 }) => {
-  return ReactDOM.createPortal(
-    <div className="z-10 absolute h-full w-full bg-modalOverlay top-0">
-      <div className="select-none relative bg-white shadow-2xl w-fit mx-auto px-8 py-8 rounded-md top-12 flex flex-col">
-        <p className="font-medium">{message || "Some error occured"}</p>
-        <button
-          className="px-4 py-2 rounded-md bg-red-600 text-white mx-auto mt-10 hover:bg-red-800"
-          onClick={() => setShowPortal(null)}
+  return (
+    <Transition appear show={true} as={Fragment}>
+      <Dialog
+        as="div"
+        className="relative z-10"
+        onClose={() => setShowPortal(null)}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
         >
-          OK
-        </button>
-      </div>
-    </div>,
-    document.body
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 overflow-y-auto">
+          <div className="flex min-h-[50ch] items-center justify-center p-4 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                <Dialog.Title
+                  as="h3"
+                  className="text-lg font-medium leading-6 text-gray-900"
+                >
+                  {message || "Some error occured"}
+                </Dialog.Title>
+                <div className="mt-4">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-red-100 px-4 py-2 text-sm font-medium text-red-900 hover:bg-red-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-500 focus-visible:ring-offset-2"
+                    onClick={() => setShowPortal(null)}
+                  >
+                    Okay
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition>
   );
 };
 export const WaringPortal: React.FC<Portalinterface> = ({
