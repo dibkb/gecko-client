7fc598fa665e6e6638db46179b6969a9fb8d4832
dibkb <dibas9110@gmail.com>
2023-03-19 10:03:52 +0530

    resizer issur

--- src/app/api/apiSlice.ts
+++ src/app/api/apiSlice.ts
@@ -1,8 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://helium-api.onrender.com/api",
-    // baseUrl: "http://localhost:8000/api",
+    // baseUrl: "https://helium-api.onrender.com/api",
+    baseUrl: "http://localhost:8000/api",
     credentials: "include",
   }),
   tagTypes: ["Blog", "User"],
