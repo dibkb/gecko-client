072d6cd78b72bdd8ef75e152a1b6eb6bfa1b197d
dibkb <dibas9110@gmail.com>
2023-03-20 13:53:14 +0530

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,53 @@
 import React from "react";
 import { Branding } from "../pages/Layout";
-
+import {
+  AiOutlineTwitter,
+  AiFillLinkedin,
+  AiOutlineGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
 const Footer: React.FC = () => {
   return (
-    <div>
-      <div className="my-10">
-        <p className="font-semibold text-2xl mb-5">Developer Insights</p>
-        <p className="">
-          Thoughts and advice on experience, learning and technology
+    <div className="mt-5 pt-5 border-t-2 border-stone-800">
+      <div>
+        <Branding />
+        <p className="text-sm font-semibold text-stone-600">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In quis lorem
+          eget elit lacinia blandit eget non arcu. Praesent mollis tortor mi,
+          nec vestibulum felis pellentesque ut.{" "}
         </p>
       </div>
-      <Branding />
+      <div className="flex gap-y-3 justify-between items-center mt-10 flex-wrap">
+        <p className="text-sm font-semibold">
+          © 2023 Helium. All rights reserved.
+        </p>
+        <main className="text-stone-700 flex gap-3">
+          <a href="https://twitter.com/dibaskb">
+            <AiOutlineTwitter
+              className="border rounded-md p-1 cursor-pointer hover:text-white hover:bg-stone-700"
+              size={"35"}
+            />
+          </a>
+          <a href="https://twitter.com/dibaskb">
+            <AiFillLinkedin
+              className="border rounded-md p-1 cursor-pointer hover:text-white hover:bg-stone-700"
+              size={"35"}
+            />
+          </a>
+          <a href="https://twitter.com/dibaskb">
+            <AiOutlineGithub
+              className="border rounded-md p-1 cursor-pointer hover:text-white hover:bg-stone-700"
+              size={"35"}
+            />
+          </a>
+          <a href="https://twitter.com/dibaskb">
+            <AiOutlineInstagram
+              className="border rounded-md p-1 cursor-pointer hover:text-white hover:bg-stone-700"
+              size={"35"}
+            />
+          </a>
+        </main>
+      </div>
     </div>
   );
 };
